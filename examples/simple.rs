bc91c834245048f8ee9529fc36c551c76980bbdc
Override-6 <maximebatista18@gmail.com>
2023-06-09 12:30:16 +0200

    change labels messages in new simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -16,9 +16,9 @@ fn main() {
         .with_message("Incompatible types")
         .with_config(Config::default().with_compact(true))
         .with_label(Label::new(0..1).with_color(Color::Red))
-        .with_label(Label::new(2..3).with_color(Color::Blue).with_message("This is another test").with_order(1))
+        .with_label(Label::new(2..3).with_color(Color::Blue).with_message("`b` for banana").with_order(1))
         .with_label(Label::new(4..5).with_color(Color::Green))
-        .with_label(Label::new(7..9).with_color(Color::Cyan).with_message("This is a test"))
+        .with_label(Label::new(7..9).with_color(Color::Cyan).with_message("`e` for emerald"))
         .finish()
         .print(Source::from(SOURCE))
         .unwrap();
