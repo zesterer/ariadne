186844aef36823f5378c30b2442319b243efcaad
Override-6 <maximebatista18@gmail.com>
2023-06-09 11:55:48 +0200

    remove empty lines for any label with no message in report

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,5 @@
-use ariadne::{Report, ReportKind, Label, Source};
+use yansi::Color;
+use ariadne::{Report, ReportKind, Label, Source, Config};
 
 fn main() {
     Report::build(ReportKind::Error, (), 34)
@@ -7,5 +8,18 @@ fn main() {
         .with_label(Label::new(42..45).with_message("This is of type Str"))
         .finish()
         .print(Source::from(include_str!("sample.tao")))
+        .unwrap();
+
+    const SOURCE: &str = "a b c d e f";
+    // also supports labels with no messages to only emphasis on some areas
+    Report::build(ReportKind::Error, (), 34)
+        .with_message("Incompatible types")
+        .with_config(Config::default().with_compact(true))
+        .with_label(Label::new(0..1).with_color(Color::Red))
+        .with_label(Label::new(2..3).with_color(Color::Blue).with_message("This is another test").with_order(1))
+        .with_label(Label::new(4..5).with_color(Color::Green))
+        .with_label(Label::new(7..9).with_color(Color::Cyan).with_message("This is a test"))
+        .finish()
+        .print(Source::from(SOURCE))
         .unwrap();
 }
