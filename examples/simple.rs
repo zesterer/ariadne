e97c3e26521765148deb888b1acbe328090fd32d
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2024-04-08 11:18:10 -0400

    - cargo clippy and cargo fmt - Clippy and fmt in CI

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,4 @@
-use ariadne::{Report, ReportKind, Label, Source, Config, Color};
+use ariadne::{Color, Config, Label, Report, ReportKind, Source};
 
 fn main() {
     Report::build(ReportKind::Error, (), 34)
@@ -15,9 +15,18 @@ fn main() {
         .with_message("Incompatible types")
         .with_config(Config::default().with_compact(true))
         .with_label(Label::new(0..1).with_color(Color::Red))
-        .with_label(Label::new(2..3).with_color(Color::Blue).with_message("`b` for banana").with_order(1))
+        .with_label(
+            Label::new(2..3)
+                .with_color(Color::Blue)
+                .with_message("`b` for banana")
+                .with_order(1),
+        )
         .with_label(Label::new(4..5).with_color(Color::Green))
-        .with_label(Label::new(7..9).with_color(Color::Cyan).with_message("`e` for emerald"))
+        .with_label(
+            Label::new(7..9)
+                .with_color(Color::Cyan)
+                .with_message("`e` for emerald"),
+        )
         .finish()
         .print(Source::from(SOURCE))
         .unwrap();
