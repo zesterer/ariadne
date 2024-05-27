7e93194d76e1606cad292fc6e97dd7d23f2c7a59
ISSOtm <me@eldred.fr>
2024-05-27 23:25:32 +0200

    Use the `format!` padding feature to pad out line numbers
    This is more compact, and it might be cheaper too!

--- src/draw.rs
+++ src/draw.rs
@@ -76,6 +76,14 @@ impl Characters {
             underline: '^',
         }
     }
+
+    pub(crate) fn vbar(&self, is_gap: bool) -> char {
+        if is_gap {
+            self.vbar_gap
+        } else {
+            self.vbar
+        }
+    }
 }
 
 /// Output stream to check for whether color is enabled.
