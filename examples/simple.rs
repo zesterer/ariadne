03256fb6c8e863bf7b32b3dcec98ba37e91e35f7
Joshua Barretto <joshua@jsbarretto.com>
2026-03-22 19:50:39 +0000

    Update example image

--- examples/simple.rs
+++ examples/simple.rs
@@ -9,34 +9,4 @@ fn main() {
         .finish()
         .print(Source::from(include_str!("sample.tao")))
         .unwrap();
-    
-    let mut colors = ColorGenerator::new();
-
-    // Generate & choose some colours for each of our elements
-    let a = colors.next();
-    let b = colors.next();
-    let out = Color::Blue;
-    
-    Report::build(ReportKind::Error, ("sample.tao", 12..12))
-        .with_code(3)
-        .with_message(format!("Incompatible types"))
-        .with_label(Label::new(("sample.tao", 32..33))
-            .with_message(format!("This is of type {}", "Nat".fg(a)))
-            .with_color(a))
-        .with_label(Label::new(("sample.tao", 52..55))
-            .with_message(format!("This is of type {}", "Str".fg(b)))
-            .with_color(b))
-        .with_label(Label::new(("sample.tao", 11..58))
-            .with_message(format!(
-                "The values are outputs of this {} expression",
-                "match".fg(out),
-            ))
-            .with_color(out))
-        .with_note(format!(
-            "Outputs of {} expressions must coerce to the same type",
-            "match".fg(out)
-        ))
-        .finish()
-        .print(("sample.tao", Source::from(include_str!("sample.tao"))))
-        .unwrap();
 }
